3c9cb5e828098c2803dd9fca3b72341dea369296
Nikolaj <nikolajkgpth@gmail.com>
2022-02-07 14:32:11 +0100

    Tilfoejelse af testkode
    Tilfoejet oled testkode til dsp

---
+++ TestWorkSpace/helloWorld/Debug/main.d
@@ -0,0 +1,87 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/string.h
+main.obj: C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/stdio.h
+main.obj: C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/ezdsp5535.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/tistdtypes.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/soc.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_sysctrl.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/csl_general.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_cpu.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_i2c.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_i2s.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_emif.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_uart.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_spi.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_mmcsd.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_lcdc.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_rtc.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_dma.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_sar.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_usb.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_gpio.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_tim.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_wdt.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/ezdsp5535_lcd.h
+main.obj: C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/ezdsp5535.h
+
+../main.c:
+
+C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/string.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/linkage.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/stdio.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/stdarg.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/ezdsp5535.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/tistdtypes.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/soc.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_sysctrl.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/csl_general.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_cpu.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_i2c.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_i2s.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_emif.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_uart.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_spi.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_mmcsd.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_lcdc.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_rtc.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_dma.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_sar.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_usb.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_gpio.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_tim.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/cslr_wdt.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/ezdsp5535_lcd.h:
+
+C:/Users/nikol/Documents/semesterKodeMapper/EIT6-WatsonC/TestWorkSpace/helloWorld/include/ezdsp5535.h:
+
---
+++ TestWorkSpace/helloWorld/Debug/src/lcdc_panel.d
@@ -0,0 +1,90 @@
+# FIXED
+
+src/lcdc_panel.obj: ../src/lcdc_panel.c
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/lcdc_panel.h
+src/lcdc_panel.obj: C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/string.h
+src/lcdc_panel.obj: C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/linkage.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/csl_lcdc.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/soc.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/tistdtypes.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_sysctrl.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/csl_general.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_cpu.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_i2c.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_i2s.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_emif.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_uart.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_spi.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_mmcsd.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_lcdc.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_rtc.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_dma.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_sar.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_usb.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_gpio.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_tim.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/cslr_wdt.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/csl_types.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/csl_error.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/csl_lcdcAux.h
+src/lcdc_panel.obj: C:/Users/nikol/workspace_v11/helloWorld/include/lcdFontTable.h
+
+../src/lcdc_panel.c:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/lcdc_panel.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/string.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/c5500_4.4.1/include/linkage.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/csl_lcdc.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/soc.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/tistdtypes.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_sysctrl.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/csl_general.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_cpu.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_i2c.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_i2s.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_emif.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_uart.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_spi.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_mmcsd.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_lcdc.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_rtc.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_dma.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_sar.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_usb.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_gpio.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_tim.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/cslr_wdt.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/csl_types.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/csl_error.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/csl_lcdcAux.h:
+
+C:/Users/nikol/workspace_v11/helloWorld/include/lcdFontTable.h:
+
