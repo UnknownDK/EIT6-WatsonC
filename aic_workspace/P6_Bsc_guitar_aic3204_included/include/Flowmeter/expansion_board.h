058b1fd8240676918acc282243332c3d31fce2a5
Mikkel Stenkjær Hansen <mha19@student.aau.dk>
2022-05-11 19:57:07 +0200

    Added expansion board function descriptions

--- aic_workspace/P6_Bsc_guitar_aic3204_included/include/Flowmeter/expansion_board.h
+++ aic_workspace/P6_Bsc_guitar_aic3204_included/include/Flowmeter/expansion_board.h
@@ -34,21 +34,64 @@ typedef struct {
 
 typedef exp_board_obj* exp_board_handle;
 
-
+/**
+ * Enable gpio and set pin directions.
+ * @param handle
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_init(exp_board_handle handle);
 
+/**
+ * Reset ADC and DAC enable pins to their initial disabled states.
+ * @param handle
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_gpio_reset_val(exp_board_handle handle);
 
+/**
+ * Enable the ADC signal to the specified transducer board.
+ * @param handle
+ * @param adc_num Transducerboard number (1-4).
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_enable_adc(exp_board_handle handle, uint16_t adc_num);
 
+/**
+ * Enable the DAC signal to the specified transducer board.
+ * @param handle
+ * @param dac_num Transducer board number (1-4).
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_enable_dac(exp_board_handle handle, uint16_t dac_num);
 
+/**
+ * Disable all ADC signals.
+ * @param handle
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_disable_adc(exp_board_handle handle);
 
+/**
+ * Disable all DAC signals.
+ * @param handle
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_disable_dac(exp_board_handle handle);
 
+/**
+ * Sets all ADC enable pin states using a bit mask.
+ * @param handle
+ * @param adc_mask Bit mask (bit 0, 1, 2 and 3) indicating the HIGH/LOW output state of the 4 enable pins.
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_adc_set(exp_board_handle handle, uint16_t adc_mask);
 
+/**
+ * Sets all DAC enable pin states using a bit mask.
+ * @param handle
+ * @param dac_mask Bit mask (bit 0, 1, 2 and 3) indicating the HIGH/LOW output state of the 4 enable pins.
+ * @return CSL success or error codes found in csl_error.h
+ */
 CSL_Status exp_board_dac_set(exp_board_handle handle, uint16_t dac_mask);
 
 #endif /* INCLUDE_FLOWMETER_EXPANSION_BOARD_H_ */
